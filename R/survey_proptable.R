155466b6dd3f2afadb250885cd62bb6a7fdef3a3
shossen1 <57533234+shossen1@users.noreply.github.com>
2020-03-14 10:38:27 -0400

    Update survey_proptable.R

--- R/survey_proptable.R
+++ R/survey_proptable.R
@@ -4,35 +4,33 @@ svy.proptable = function(data = data,
                          strata = "strata",
                          outcome = "outcome",
                          predictors = varlist,
-                         row_column = 1,
                          tablename = tablename){
   
-    if (!requireNamespace("survey", quietly = TRUE)) {
+  if (!requireNamespace("survey", quietly = TRUE)) {
     stop("Package \"survey\" needed for this function to work. Please install it.",
-      call. = FALSE)
+         call. = FALSE)
   }
   
-      if (!requireNamespace("DescTools", quietly = TRUE)) {
+  if (!requireNamespace("DescTools", quietly = TRUE)) {
     stop("Package \"DescTools\" needed for this function to work. Please install it.",
-      call. = FALSE)
+         call. = FALSE)
   }
+  
 
-library(survey);library(DescTools)
-  # Change the svydesign according to your data
-svydesign <- svydesign(ids=~psu, data=data, weights=~weights, strata = ~strata)
-
+  library(survey);library(DescTools)
+  svydesign <- eval(parse(text=paste0("svydesign(ids=~",psu, ",data=data", ",weights=~",weights, ",strata=~",strata,")",sep="")))
 
   total_category = length(table(data[[outcome]]))
   tab = data.frame()
-
+  
   for (i in predictors){
     length = length(table(data[[i]]))+1
     tab0 = as.data.frame(matrix(ncol = total_category+1, nrow = 0))
-
+    
     for (j in c(1:length(table(data[[i]])))){
       observed = svytable(as.formula(paste0("(~",outcome,"+",i,")",sep="")),svydesign)[,j]
       total = sum(observed)
-      prop= (DescTools::BinomCI(observed, total, conf.level = 0.95, method = "logit")*100)
+      prop= (BinomCI(observed, total, conf.level = 0.95, method = "logit")*100)
       tab0[1,1] = i
       tab0[1,c(1:total_category+1)] = ""
       tab0[c(2:length),1] = rownames(table(data[[i]]))
